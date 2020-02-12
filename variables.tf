b54f9e41363540de89eccbd75ede1d6a02f6abe2
Partha <parthasaradhi.abdas@gsa.gov>
2020-02-12 00:54:27 -0600

    adding circle ci config file

--- variables.tf
+++ variables.tf
@@ -3,42 +3,21 @@ variable "guardduty_bucket_name" {
   description = "(required) The name given to the primary logging bucket"
 }
 
+variable "guardduty_bucket_acl" {
+  description = "(optional) The ACL applied to the primary logging bucket"
+  default     = "log-delivery-write"
+}
+
 variable "guardduty_bucket_destroy" {
   type        = bool
   description = "(optional) The boolean value indicating whether Terraform can destroy the bucket with objects still inside"
   default     = false
 }
 
-variable "guardduty_access_guardduty_prefix" {
-  description = "(optional) The prefix used when storing access logs for the logging bucket"
-  default     = "grace-logging"
-}
-
-variable "guardduty_bucket_acl" {
-  description = "(optional) The ACL applied to the primary logging bucket"
-  default     = "log-delivery-write"
-}
-
 variable "guardduty_bucket_enable_versioning" {
   type        = bool
   description = "(optional) The boolean value enabling (true) or disabling (false) versioning on the logging bucket"
   default     = true
-}
-
-variable "guardduty_bucket_enable_backup" {
-  type        = bool
-  description = "(optional) The boolean value enabling (true) or disabling (false) backups to glacier on the logging bucket"
-  default     = true
-}
-
-variable "guardduty_bucket_backup_days" {
-  description = "(optional) The age of an object in number of days before it can be archived to glacier"
-  default     = "365"
-}
-
-variable "guardduty_bucket_backup_expiration_days" {
-  description = "(optional) The age of an object in number of days before it can be safely discarded"
-  default     = "900"
 }
 
 variable "guardduty_bucket_block_public_acls" {
